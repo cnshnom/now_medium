827bbcd383a5f236e1210a4991298e8c9c7dc6a1
cnshnom <yupeiyu.cns.hn@gmail.com>
2022-06-05 23:44:30 +0200

    heroku debug

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,9 +4,9 @@ class PagesController < ApplicationController
 
   def index
     # @stories = Story.order(created_at: :desc).includes(:user)
-    # @stories = Story.where(status: 'published').order(created_at: :desc).includes(:user)
+     @stories = Story.where(status: 'published').order(created_at: :desc).includes(:user)
     # @stories = Story.published_stories.order(created_at: :desc).includes(:user)
-     @stories = Story.published.with_attached_cover_image.order(created_at: :desc).includes(:user)
+   #  @stories = Story.published.with_attached_cover_image.order(created_at: :desc).includes(:user)
     #@stories = Story.published_stories
   end
 
